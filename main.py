7308b34b7f85b6060f190fa3852bd94a32104284
carali6 <carali6@outlook.com>
2024-04-12 18:28:31 -0400

    completed version

--- main.py
+++ main.py
@@ -10,37 +10,58 @@ def encode():
     result = ""
     for letter in password:
         if letter == "0":
-            result += "2"
+            result += "3"
         elif letter == "1":
             result += "4"
         elif letter == "2":
-            result += "4"
-        elif letter == "3":
             result += "5"
-        elif letter == "4":
+        elif letter == "3":
             result += "6"
-        elif letter == "5":
+        elif letter == "4":
             result += "7"
-        elif letter == "6":
+        elif letter == "5":
             result += "8"
-        elif letter == "7":
+        elif letter == "6":
             result += "9"
-        elif letter == "8":
+        elif letter == "7":
             result += "0"
-        elif letter == "9":
+        elif letter == "8":
             result += "1"
+        elif letter == "9":
+            result += "2"
     print("Your password has been encoded and stored!")
     return result
 
-def decode(encoded_password):
-    return 1
-    #fill in the decoded function
-
+def decode(result):
+    # fill in the decoded function
+    final = ""
+    for digit in result:
+        if digit == "0":
+            final += "7"
+        elif digit == "1":
+            final += "8"
+        elif digit == "2":
+            final += "9"
+        elif digit == "3":
+            final += "0"
+        elif digit == "4":
+            final += "1"
+        elif digit == "5":
+            final += "2"
+        elif digit == "6":
+            final += "3"
+        elif digit == "7":
+            final += "4"
+        elif digit == "8":
+            final += "5"
+        elif digit == "9":
+            final += "6"
+    return final
 
 
 def main():
-    menu()
     while True:
+        menu()
         option = input("Please enter an option: ")
         if option == "1":
             encoded_password = encode()
